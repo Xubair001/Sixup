f42984ecc0e006538b44f09ac3d2e963880b5eda
Abdullah Zubair <abdullah.zubair@codeaza.org>
2026-06-15 17:45:45 +0500

    Add Dockerignore files for backend and frontend; update Dockerfile for multi-stage build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       { protocol: "http", hostname: "localhost", port: "8001" },
+      { protocol: "https", hostname: "*.amazonaws.com" },
     ],
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
